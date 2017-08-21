9ebafefff7644f1ed3bf12f6ea80b3544994dcdb
Alex Spinnler <alex@makeitmobile.co>
2017-08-21 13:56:54 -0500

    first pass at Phase 1 of CAShapeLayerTutorial. Add UITableViewCell, images, app icon and text

--- CAShapeLayerExample/CAShapeLayerExample/ViewController.swift
+++ CAShapeLayerExample/CAShapeLayerExample/ViewController.swift
@@ -1,18 +1,37 @@
-//
-//  ViewController.swift
-//  CAShapeLayerExample
-//
-//  Created by Damien Bell on 8/19/17.
-//  Copyright © 2017 MakeItMobile. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let data = [
+        (
+            title: "Winter is coming!",
+            subtitle: "Under new management",
+            description: "Now with 10% fewer flayings. Come for the magnificant wildlife: dire wolves, wogs, and the walking dead.",
+            imageName: "blue-crystal"
+        ),
+        (
+            title: "It's Always Sunny in Dorne",
+            subtitle: "Nuts to Winterfell",
+            description: "Take a break from White Walkers, ravens and prudish royality. Live out winter in style.",
+            imageName: "ocean"
+        ),
+        (
+            title: "Got Dragonglass?",
+            subtitle: "We've got tons of it!",
+            description: "Are you tired of metals tools? Long for the days when things were made of good ol' dependable rocks?",
+            imageName: "dragonstone"
+        ),
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.register( UINib(nibName: "StyledTableViewCell", bundle: nil), forCellReuseIdentifier: "StyledTableViewCell")
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +40,32 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = self.tableView.dequeueReusableCell(withIdentifier: "StyledTableViewCell") as? StyledTableViewCell else { return UITableViewCell() }
+        let item = self.data[ indexPath.row]
+        
+        cell.titleLabel.text = item.title
+        cell.subtitleLabel.text = item.subtitle
+        cell.descriptionLabel.text = item.description
+        cell.backgroundImage?.image = UIImage(named: item.imageName)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.data.count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return tableView.frame.height/CGFloat(self.data.count)
+    }
 }
 
